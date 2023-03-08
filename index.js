9197cfe620b3be1694928428375fb471f3f30dce
IOINITID <50173108+IOINITID@users.noreply.github.com>
2023-03-08 22:24:34 +0300

    Added args validation

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("LOG: Application start.");
+#!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+
+  console.log("LOG: CLI args:", args);
+
+  if (args.help) {
+    // TODO: Вывести help в консоль
+  }
+
+  if (args.city) {
+    // TODO: Сохранить город
+  }
+
+  if (args.token) {
+    // TODO: Сохранить токен
+  }
+};
+
+initCLI();
