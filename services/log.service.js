c91587d90b04f60d7837fdea6a8788d9afd81d18
ioinitid <ioinitid@gmail.com>
2023-03-10 11:50:02 +0300

    Added log service

---
+++ services/log.service.js
@@ -0,0 +1,24 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = (error) => {
+  console.log(chalk.bgRed(' Error ') + ' ' + error);
+};
+
+const printSuccess = (message) => {
+  console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+};
+
+const printHelp = () => {
+  console.log(
+    dedent`
+    ${chalk.bgCyan(' HELP ')}
+    Без параметров - вывод погоды
+    -c [CITY] для установки города
+    -h для вывода помощи
+    -t [API_KEY] для сохранения токена
+    `
+  );
+};
+
+export { printError, printSuccess, printHelp };
