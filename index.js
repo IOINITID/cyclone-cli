908efd6858d0362648616aaa6a5641254dc7d847
ioinitid <ioinitid@gmail.com>
2023-03-10 12:46:48 +0300

    Added storage service

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import dedent from 'dedent-js';
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Токен сохранен');
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
 
-  console.log('LOG: CLI args:', args);
+  console.log(
+    dedent`${chalk.bgYellowBright(' LOG ')} Переданные аргументы:
+  `,
+    args
+  );
 
   if (args.h) {
     printHelp();
@@ -16,7 +32,7 @@ const initCLI = () => {
   }
 
   if (args.t) {
-    // TODO: Сохранить токен
+    return saveToken(args.t);
   }
 
   // TODO: Вывести погоду
