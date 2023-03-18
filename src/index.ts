39a135d46ad86a3ebad383334127f8ed2473ef20
ioinitid <ioinitid@gmail.com>
2023-03-18 17:12:22 +0300

    Added cyclone controller

--- src/index.ts
+++ src/index.ts
@@ -1,125 +1,8 @@
 #!/usr/bin/env node
-import { AxiosError } from 'axios';
-import { apiService } from './services/api.service';
 import { printService } from './services/print.service';
-import { Storage, storageService } from './services/storage.service';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
-import { createSpinner } from 'nanospinner';
-import inquirer from 'inquirer';
-
-// TODO: Добавить в cyclone controller
-export const saveToken = async (token: string) => {
-  const spinner = createSpinner();
-
-  if (token === 'true') {
-    spinner.error({ text: 'Токен не передан...' });
-
-    return;
-  }
-
-  try {
-    spinner.start({ text: 'Сохранение токена...' });
-
-    await storageService.set(Storage.Token, token);
-
-    spinner.success({ text: 'Токен сохранен...' });
-  } catch (error) {
-    spinner.error({ text: 'Ошибка сохранения токена...' });
-  }
-};
-
-// TODO: Добавить в cyclone controller
-export const saveCity = async (city: string) => {
-  const spinner = createSpinner();
-
-  if (city === 'true') {
-    spinner.error({ text: 'Город не передан...' });
-
-    return;
-  }
-
-  try {
-    spinner.start({ text: 'Сохранение города...' });
-
-    await storageService.set(Storage.City, city);
-
-    spinner.success({ text: 'Город сохранен...' });
-  } catch (error) {
-    spinner.error({ text: 'Ошибка сохранения города...' });
-  }
-};
-
-// TODO: Добавить в cyclone controller
-const getWeatherForecast = async () => {
-  const spinner = createSpinner();
-
-  try {
-    let token = await storageService.get(Storage.Token);
-    let city = await storageService.get(Storage.City);
-
-    // TODO: Вынести проверуки в отдельный метод
-    if (!token) {
-      const prompt = inquirer.createPromptModule();
-
-      const inputToken = await prompt({
-        type: 'input',
-        name: 'token',
-        message: 'Добавьте ключ API полученный с сервиса https://openweathermap.org:',
-      });
-
-      if (inputToken.token) {
-        await saveToken(inputToken.token);
-        token = await storageService.get(Storage.Token);
-      } else {
-        spinner.error({
-          text: 'Не добавлен ключ API, повторите запрос или добавьте его через команду -t, --token [API_KEY]',
-        });
-        return;
-      }
-    }
-
-    // TODO: Вынести проверуки в отдельный метод
-    if (!city) {
-      const prompt = inquirer.createPromptModule();
-
-      const inputCity = await prompt({
-        type: 'input',
-        name: 'city',
-        message: 'Добавьте город:',
-      });
-
-      if (inputCity.city) {
-        await saveCity(inputCity.city);
-        city = await storageService.get(Storage.City);
-      } else {
-        spinner.error({
-          text: 'Город не добавлен, повторите запрос или добавьте его через команду -c, --city [CITY]',
-        });
-        return;
-      }
-    }
-
-    spinner.start({ text: 'Загрузка...' });
-
-    const weather = await apiService.getWeather(token, city);
-
-    spinner.success({ text: 'Загружено...' });
-
-    printService.weather(weather, apiService.getWeatherIcon(weather.weather[0].icon) as string);
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      if (error.response?.status === 404) {
-        spinner.error({ text: 'Неверно указан город, обновите его через команду -c, --city [CITY]' });
-      } else if (error.response?.status === 401) {
-        spinner.error({ text: 'Неверно указан токен, обновите его через команду -t, --token [API_KEY]' });
-      }
-      return;
-    }
-
-    spinner.error({ text: 'Ошибка загрузки...' });
-  }
-};
+import { cycloneController } from './controllers/cyclone.controller';
 
 const main = async () => {
   const args = await yargs(hideBin(process.argv)).help(false).version(false).argv;
@@ -134,25 +17,25 @@ const main = async () => {
 
   if (args.c || args.city) {
     if (args.c) {
-      return saveCity(String(args.c));
+      return cycloneController.saveCity(String(args.c));
     }
 
-    return saveCity(String(args.city));
+    return cycloneController.saveCity(String(args.city));
   }
 
   if (args.t || args.token) {
     if (args.t) {
-      return saveToken(String(args.t));
+      return cycloneController.saveToken(String(args.t));
     }
 
-    return saveToken(String(args.token));
+    return cycloneController.saveToken(String(args.token));
   }
 
   if (args.v || args.version) {
     return printService.version();
   }
 
-  return getWeatherForecast();
+  return cycloneController.getWeatherForecast();
 };
 
 main();
